f769f3392b49f824ecd989f64b5b63ed7ab3b7dd
N1k78 <nikita.d4444@gmail.com>
2025-07-07 16:12:13 +0300

    have probams

--- Tetris/paint.py
+++ Tetris/paint.py
@@ -1,7 +1,9 @@
 import pygame
 import sys
 from generait import generait_map,set_color,do_collor,map
-from sprites import get_data
+from sprites import SHAPES
+import random
+from time import sleep
 
 def number_to_color(number)->list:
     if number == 1:
@@ -23,43 +25,68 @@ def paint_map(screen,map):
     global X,Y,stor
     h = len(map)    # висота 
     l = len(map[0]) # довжина
-    print("h = ",h)
-    print("l = ",l)
+    # print("h = ",h)
+    # print("l = ",l)
+    screen.fill(pygame.Color("green"))
     for iL in range(0,l):
         for iH in range(0,h):
-            inn,out = number_to_color(map[iH][iL])
-            pygame.draw.rect(screen, (out), (X+int(stor*iL), Y+int(stor*iH), stor, stor))
-            pygame.draw.rect(screen, (inn), (X+int(stor*iL)+1, Y+int(stor*iH)+1, stor-2, stor-2))
+            pygame.draw.rect(screen, ((255,255,255)), (X+int(stor*iL), Y+int(stor*iH), stor, stor))
+            pygame.draw.rect(screen, ((0,0,0)), (X+int(stor*iL)+1, Y+int(stor*iH)+1, stor-2, stor-2))
 
-def paint_shape(sprite,x_position,y_position): # намалювати фігуру
-    color,shape = get_data(sprite)
+def paint_shape(shape,color,x_position,y_position): # намалювати фігуру
+    global surface
+    global screen
     for hight in range(0,len(shape)):
-        print(shape[hight])
         for lenght in range(0,len(shape[hight])):
             if shape[hight][lenght] == 1:
                 rect = pygame.Rect(x_position * stor + stor * lenght, y_position * stor + stor * hight, stor, stor) 
                 pygame.draw.rect(surface, color, rect)
     screen.blit(surface, (Y, X))
-    
+
+def move(data,dx,dy):
+    global active
+    name = data["name"]
+    color = SHAPES[name]["color"]
+    rotation = data["rotation"]
+    shape = SHAPES[name]["patterns"][rotation]
+    if (active["x"] + len(shape[0])  < L) and dx > 0:
+        active["x"] = data["x"]+dx
+    if (active["x"] >= 1) and dx < 0:
+        active["x"] = data["x"] + dx
+    active["y"] = int(data["y"])+dy
+
+def spawn_pies() -> dict:
+    global L
+    name = random.choice(list(SHAPES.keys()))
+    SHAPES_data = SHAPES[name]
+    x = L // 2 - len(SHAPES_data['patterns'][0][0]) // 2
+    base ={
+        'name' : name,
+        'patterns' : SHAPES_data['patterns'][0],
+        'rotation' : 0,
+        'x' : x,
+        'y' : "0"
+    }
+    return base   
 
 if __name__ == "__main__":
     stor = 20
-    h = 20
-    l = 10
+    H = 20
+    L = 10
     X = 20
     Y = 20
-    generait_map(h,l)
+    generait_map(H,L)
     # set_color(2,4,do_collor())
     # for rous in range(0,len(map)):
     #     print(map[rous])
 
     pygame.init()
-    screen = pygame.display.set_mode((20*(l+2)+100, 20*(h+2)))
+    screen = pygame.display.set_mode((20*(L+2)+100, 20*(H+2)))
     pygame.display.set_caption('Tetris')
     screen.fill(pygame.Color("black"))
     
-    surface_width = l * stor
-    surface_height = h * stor
+    surface_width = L * stor
+    surface_height = H * stor
     surface = pygame.Surface((surface_width, surface_height), pygame.SRCALPHA)
     
     paint_map(screen,map)
@@ -67,12 +94,34 @@ if __name__ == "__main__":
     sprite = "I"
     x_position = 2
     y_position = 3
-    paint_shape(sprite,x_position,y_position)
+    # paint_shape(SHAPES[sprite]["patterns"][0],SHAPES[sprite]["color"],x_position,y_position)
 
-    pygame.display.flip()
+    active = spawn_pies()
+    print(active)
+    
+    move(active,-1,1)
     
     while True:
         for event in pygame.event.get():
+            keboard = event.type
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            elif keboard == pygame.KEYDOWN:
+                keboard = event.key
+                if keboard == pygame.K_LEFT:
+                    print("1")
+                    move(active,-1,0)
+                elif keboard == pygame.K_RIGHT:
+                    print("2")
+                    move(active,1,0)
+                elif keboard == pygame.K_DOWN:
+                    print("3")
+                    move(active,0,1)
+                elif keboard == pygame.K_UP:
+                    active["rotation"] = (active["rotation"]+1)%4
+        paint_map(screen,map)
+        paint_shape(SHAPES[active["name"]]["patterns"][active["rotation"]],SHAPES[sprite]["color"],active["x"],active["y"])
+        pygame.display.flip()
+        screen.fill(pygame.Color("green"))
+        sleep(0.1)
--- Tetris/sprites.py
+++ Tetris/sprites.py
@@ -1,56 +1,131 @@
+import random
+
 SHAPES = {
     'I': {
-        'pattern': [
+        'patterns': [[
             [1, 1, 1, 1]
         ],
+        [
+            [1],
+            [1],
+            [1],
+            [1]
+        ]
+        ],
+        'rotations_count': 2,
         'color': (0, 255, 255)  # Cyan / Ціановий
     },
     'O': {
-        'pattern': [
-            [1, 1],
-            [1, 1]
+        'patterns': [
+            [
+                [1, 1],
+                [1, 1]
+            ]
         ],
+        'rotations_count': 1,
         'color': (255, 255, 0)  # Yellow / Жовтий
     },
     'T': {
-        'pattern': [
+        'patterns': [[
             [0, 1, 0],
             [1, 1, 1]
         ],
+        [
+            [1, 0],
+            [1, 1],
+            [1, 0]
+        ],
+        [
+            [1, 1, 1],
+            [0, 1, 0]
+        ],
+        [
+            [0, 1],
+            [1, 1],
+            [0, 1]
+        ]
+        ],
+        'rotations_count': 4,
         'color': (128, 0, 128)  # Purple / Фіолетовий
     },
     'S': {
-        'pattern': [
+        'patterns': [[
             [0, 1, 1],
             [1, 1, 0]
         ],
+        [
+            [1, 0],
+            [1, 1],
+            [0, 1]
+        ]
+        ],
+        'rotations_count': 2,
         'color': (0, 255, 0)  # Green / Зелений
     },
     'Z': {
-        'pattern': [
+        'patterns': [[
             [1, 1, 0],
             [0, 1, 1]
         ],
+        [
+            [0, 1],
+            [1, 1],
+            [1, 0]
+        ]
+        ],
+        'rotations_count': 2,
         'color': (255, 0, 0)  # Red / Червоний
     },
     'J': {
-        'pattern': [
+        'patterns': [[
             [1, 0, 0],
             [1, 1, 1]
         ],
+        [
+            [1, 1],
+            [1, 0],
+            [1, 0]
+        ],
+        [
+            [1, 1, 1],
+            [0, 0, 1]
+        ],
+        [
+            [1, 0],
+            [1, 0],
+            [1, 1]
+        ]
+        ],
+        'rotations_count': 4,
         'color': (0, 0, 255)  # Blue / Синій
     },
     'L': {
-        'pattern': [
+        'patterns': [[
             [0, 0, 1],
             [1, 1, 1]
         ],
+        [
+            [1, 0],
+            [1, 0],
+            [1, 1]
+        ],
+        [
+            [1, 1, 1],
+            [1, 0, 0]
+        ],
+        [
+            [1, 1],
+            [0, 1],
+            [0, 1]
+        ]
+        ],
+        'rotations_count': 4,
         'color': (255, 165, 0)  # Orange / Помаранчевий
     }
 }
 
+                                                      
+
 def get_data(shape_name): # встановити дані за фигури shape_name
     shape_data = SHAPES[shape_name]
-    pattern = shape_data['pattern']
-    color = shape_data['color']
-    return color, pattern
+    return shape_data
